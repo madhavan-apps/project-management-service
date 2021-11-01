51f1412d1ba79b58b94a634293469158cf7f6041
samlearninghub <54409434+samlearninghub@users.noreply.github.com>
2021-11-01 07:42:43 +0530

    code commit
    code commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:12-jdk-alpine
 VOLUME /tmp
 EXPOSE 5000
-ADD project-management-service-0.0.1-SNAPSHOT.jar proj-mgmt-service.jar
+ADD build/libs/*.jar proj-mgmt-service.jar
 ENV JAVA_OPS=""
 ENTRYPOINT ["java", "-jar", "/proj-mgmt-service.jar"]
--- src/main/java/com/proj/mgmt/rest/controller/OpsController.java
+++ src/main/java/com/proj/mgmt/rest/controller/OpsController.java
@@ -6,6 +6,7 @@ import static com.proj.mgmt.common.OPSConstants.RESP_CODE_MSG_MAP.SUCCESS;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import com.proj.mgmt.rest.Response;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("comp")
